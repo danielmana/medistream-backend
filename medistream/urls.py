7c0b1ace6f90e6befddf3d0d1123f951ea3d20a9
Daniel M <dmana@flumotion.com>
2014-06-19 14:12:48 +0200

    feat(user): new view to create a user with no authentication required

--- medistream/urls.py
+++ medistream/urls.py
@@ -3,14 +3,15 @@ from rest_framework.routers import DefaultRouter
 from medistream import views
 
 
+urlpatterns = patterns('',
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
+    url(r'^users/register', 'medistream.views.register')
+)
+
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'events', views.EventViewSet)
 router.register(r'talks', views.TalkViewSet)
 router.register(r'organizers', views.OrganizerViewSet)
 router.register(r'speakers', views.SpeakerViewSet)
-urlpatterns = router.urls
-
-urlpatterns += patterns('',
-    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token')
-)
+urlpatterns += router.urls
--- medistream/views.py
+++ medistream/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import permissions
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from medistream.serializers import *
 from medistream.filters import *
 
@@ -8,6 +14,24 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+@api_view(['POST'])
+def register(request):
+    valid_user_fields = [f.name for f in get_user_model()._meta.fields]
+    defaults = {
+        # you can define any defaults that you would like for the user, here
+    }
+    serialized = UserSerializer(data=request.DATA)
+    if serialized.is_valid():
+        user_data = {field: data for (field, data) in request.DATA.items() if field in valid_user_fields}
+        user_data.update(defaults)
+        user = get_user_model().objects.create_user(
+            **user_data
+        )
+        return Response(UserSerializer(instance=user).data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class EventViewSet(viewsets.ReadOnlyModelViewSet):
